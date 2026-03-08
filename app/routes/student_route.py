8716fa4c49cc3afe885993f5e7bc1e5a8aac6338
Hunsrengking <hunsreng2021@gmail.com>
2026-03-09 01:19:14 +0700

    change end poin

--- app/routes/student_route.py
+++ app/routes/student_route.py
@@ -4,30 +4,30 @@ from app.config.db import get_db
 from app.controllers.student_controller import StudentController
 from app.models.student_model import StudentCreate, StudentResponse, StudentUpdate
 
-router = APIRouter(prefix="/api/students", tags=["Students"])
+router = APIRouter(prefix="/api", tags=["Students"])
 
 
-@router.post("/", response_model=StudentResponse)
+@router.post("/students", response_model=StudentResponse)
 def create_student(student: StudentCreate, db: Session = Depends(get_db)):
     return StudentController.create(student, db)
 
 
-@router.get("/", response_model=list[StudentResponse])
+@router.get("/students", response_model=list[StudentResponse])
 def get_students(db: Session = Depends(get_db)):
     return StudentController.get_all(db)
 
 
-@router.get("/{student_id}", response_model=StudentResponse)
+@router.get("/students/{student_id}", response_model=StudentResponse)
 def get_student(student_id: int, db: Session = Depends(get_db)):
     return StudentController.get_by_id(student_id, db)
 
 
-@router.put("/{student_id}", response_model=StudentResponse)
+@router.put("/students/{student_id}", response_model=StudentResponse)
 def update_student(
     student_id: int, student: StudentUpdate, db: Session = Depends(get_db)
 ):
     return StudentController.update(student_id, student, db)
 
-@router.delete("/{student_id}")
+@router.delete("/students/{student_id}")
 def delete_student(student_id: int, db: Session = Depends(get_db)):
     return StudentController.delete(student_id, db)
